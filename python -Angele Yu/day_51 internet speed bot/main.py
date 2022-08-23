6c8fe1b0cff7347d2ac35da008551f3254e0fe49
sarthakpatel11 <111882748+sarthakpatel11@users.noreply.github.com>
2022-08-23 22:15:14 +0530

    Update main.py

--- python -Angele Yu/day_51 internet speed bot/main.py
+++ python -Angele Yu/day_51 internet speed bot/main.py
@@ -4,9 +4,9 @@ import time
 import smtplib
 
 MIN_SOEED = 10
-sender_email = "mastimajaknahikarva@gmail.com"
-password = "M@stiM@j@K1"
-resever_email = "2maxjohnny292@gmail.com"
+sender_email = "mail"
+password = "pass"
+resever_email = "mail"
 
 chrome_driver_path = "D:/Learning Stuff/python/python -Angele Yu/devlopment/chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
